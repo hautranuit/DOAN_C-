900b22a0d856eac9772e04f39c545a23019c8770
Tran Ngoc Hau <22520412@gm.uit.edu.vn>
2024-05-30 22:43:47 +0700

    Init Project

--- CinemaManagement_Project/QuenMatKhau.Designer.cs
+++ CinemaManagement_Project/QuenMatKhau.Designer.cs
@@ -39,11 +39,12 @@
             // 
             button2.BackColor = Color.DarkSlateBlue;
             button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
             button2.ForeColor = SystemColors.ButtonHighlight;
-            button2.Location = new Point(226, 347);
+            button2.Location = new Point(258, 463);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(431, 49);
+            button2.Size = new Size(493, 65);
             button2.TabIndex = 16;
             button2.Text = "Send your new password via email";
             button2.UseVisualStyleBackColor = false;
@@ -52,56 +53,58 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F);
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             label2.ForeColor = Color.Silver;
-            label2.Location = new Point(155, 223);
+            label2.Location = new Point(177, 297);
             label2.Name = "label2";
             label2.RightToLeft = RightToLeft.Yes;
-            label2.Size = new Size(107, 21);
+            label2.Size = new Size(134, 28);
             label2.TabIndex = 15;
             label2.Text = "Email đăng ký";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 20F);
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(303, 150);
+            label1.Location = new Point(346, 200);
             label1.Name = "label1";
-            label1.Size = new Size(260, 37);
+            label1.Size = new Size(328, 46);
             label1.TabIndex = 13;
             label1.Text = "Reset your password";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("MS UI Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Font = new Font("MS UI Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point);
             label6.ForeColor = Color.Navy;
-            label6.Location = new Point(363, 81);
+            label6.Location = new Point(415, 108);
             label6.Name = "label6";
-            label6.Size = new Size(137, 24);
+            label6.Size = new Size(173, 30);
             label6.TabIndex = 37;
             label6.Text = "Get Started";
             // 
             // Email_Dang_Ky
             // 
-            Email_Dang_Ky.Location = new Point(273, 216);
+            Email_Dang_Ky.Location = new Point(312, 288);
+            Email_Dang_Ky.Margin = new Padding(3, 4, 3, 4);
             Email_Dang_Ky.Multiline = true;
             Email_Dang_Ky.Name = "Email_Dang_Ky";
             Email_Dang_Ky.PasswordChar = '*';
-            Email_Dang_Ky.Size = new Size(322, 36);
+            Email_Dang_Ky.Size = new Size(367, 47);
             Email_Dang_Ky.TabIndex = 38;
             // 
             // QuenMatKhau
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(986, 600);
             Controls.Add(Email_Dang_Ky);
             Controls.Add(label6);
             Controls.Add(button2);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "QuenMatKhau";
             Text = "Form1";
             ResumeLayout(false);
